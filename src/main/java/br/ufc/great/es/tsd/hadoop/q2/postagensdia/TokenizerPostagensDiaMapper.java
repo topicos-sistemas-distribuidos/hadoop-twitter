9516d57298570d251e54f2f7116ee632a14a9d4c
armandossrecife <armando.sousa@gmail.com>
2019-05-30 22:05:05 -0300

    Criado um primeiro prototipo do Postagens por dia.

--- src/main/java/br/ufc/great/es/tsd/hadoop/q2/postagensdia/TokenizerPostagensDiaMapper.java
+++ src/main/java/br/ufc/great/es/tsd/hadoop/q2/postagensdia/TokenizerPostagensDiaMapper.java
@@ -1,6 +1,11 @@
 package br.ufc.great.es.tsd.hadoop.q2.postagensdia;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,8 +20,44 @@ public class TokenizerPostagensDiaMapper extends Mapper<Object, Text, Text, Text
 		linha = value.toString();
 
 		if (linha != null) {
-			
-		}
-	}
+			// Utiliza a funcao split da versao jdk1.4
+			String[] tokens = linha.split("\t");
 
+			// separa por espacos em branco o token 1 - comentario
+			StringTokenizer itr = new StringTokenizer(tokens[1], " ");
+			SimpleDateFormat formato2 = new SimpleDateFormat("dd");
+
+			while (itr.hasMoreTokens()) {
+				String myKey = itr.nextToken();
+
+				// verifica se o token contem hashtag
+				if (myKey.contains("#")) {
+					myKey = myKey.replace("\"", "").replace(".", "");
+					String[] tokensAux = myKey.split("#");
+					for(int i=0; i<tokensAux.length; i++) {
+						if (!tokensAux[i].equals("")){
+							// separa o token 5 - data
+							String[] tokensData = tokens[7].split(" ");
+							// pega apenas o indice 2 - dia - para adicionar ao hashmap
+							Date dataFormatada = null;
+							try {
+								dataFormatada = formato2.parse(tokensData[2]);
+							} catch (ParseException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							GregorianCalendar calendar = new GregorianCalendar();
+							calendar.setTime(dataFormatada);
+							int dia = calendar.get(GregorianCalendar.DAY_OF_MONTH);
+							word.set("#"+tokensAux[i]);
+							one.set(String.valueOf(dia));
+							context.write(word, one);
+						}
+					}
+				}
+
+			}
+		}
+
+	}
 }
